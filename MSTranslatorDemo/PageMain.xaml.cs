fae8f430f278b1fc155f5efee7b3d5191360b8a6
LeeGray1 <s00188181@mail.itsligo.ie>
2020-06-05 17:56:40 +0100

    Error on translated invoice button

--- MSTranslatorDemo/PageMain.xaml.cs
+++ MSTranslatorDemo/PageMain.xaml.cs
@@ -69,224 +69,34 @@ namespace MSTranslatorDemo
             }
 
         }
-        //private void GetLanguagesForTranslate()
-        //{
-        //    Send request to get supported language codes
-        //    string uri = String.Format(TEXT_TRANSLATION_API_ENDPOINT, "languages") + "&scope=translation";
-        //    WebRequest webRequest = WebRequest.Create(uri);
-        //    WebRequest.Headers.Add("Accept-Language", "en");
-        //    WebResponse response = null;
-        //    Read and parse the JSON response
-        //    response = WebRequest.GetResponse();
-        //    using (var reader = new StreamReader(response.GetResponseStream(), UnicodeEncoding.UTF8))
-        //    {
-        //        var result = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, Dictionary<string, string>>>>(reader.ReadToEnd());
-        //        var languages = result["translation"];
-
-        //        languageCodes = languages.Keys.ToArray();
-        //        foreach (var kv in languages)
-        //        {
-        //            languageCodesAndTitles.Add(kv.Value["name"], kv.Key);
-        //        }
-        //    }
-        //}
-        // NOTE:
-        // In the following sections, we'll add code below this.
+       
+       
         private void PopulateLanguageMenus()
         {
             // Add option to automatically detect the source language
             FromLanguageComboBox.Items.Add("Detect");
+            List<string> list = new LanguageClass().FillLanguages();            
 
-            int count = languageCodesAndTitles.Count;
-            foreach (string menuItem in languageCodesAndTitles.Keys)
-            {
-                FromLanguageComboBox.Items.Add(menuItem);
-                ToLanguageComboBox.Items.Add(menuItem);
-            }
+            ToLanguageComboBox.ItemsSource = list;
 
             // Set default languages
-            FromLanguageComboBox.SelectedItem = "Detect";
+            
             ToLanguageComboBox.SelectedItem = "English";
         }
         // NOTE:
         // In the following sections, we'll add code below this.
         // ***** DETECT LANGUAGE OF TEXT TO BE TRANSLATED
-        private string DetectLanguage(string text)
-        {
-            string detectUri = string.Format(TEXT_TRANSLATION_API_ENDPOINT, "detect");
-
-            // Create request to Detect languages with Translator
-            HttpWebRequest detectLanguageWebRequest = (HttpWebRequest)WebRequest.Create(detectUri);
-            detectLanguageWebRequest.Headers.Add("Ocp-Apim-Subscription-Key", COGNITIVE_SERVICES_KEY);
-            detectLanguageWebRequest.Headers.Add("Ocp-Apim-Subscription-Region", "westeurope");
-            detectLanguageWebRequest.ContentType = "application/json; charset=utf-8";
-            detectLanguageWebRequest.Method = "POST";
-
-            // Send request
-            var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
-            string jsonText = serializer.Serialize(text);
-
-            string body = "[{ \"Text\": " + jsonText + " }]";
-            byte[] data = Encoding.UTF8.GetBytes(body);
-
-            detectLanguageWebRequest.ContentLength = data.Length;
-
-            using (var requestStream = detectLanguageWebRequest.GetRequestStream())
-                requestStream.Write(data, 0, data.Length);
-
-            HttpWebResponse response = (HttpWebResponse)detectLanguageWebRequest.GetResponse();
-
-            // Read and parse JSON response
-            var responseStream = response.GetResponseStream();
-            var jsonString = new StreamReader(responseStream, Encoding.GetEncoding("utf-8")).ReadToEnd();
-            dynamic jsonResponse = serializer.DeserializeObject(jsonString);
-
-            // Fish out the detected language code
-            var languageInfo = jsonResponse[0];
-            if (languageInfo["score"] > (decimal)0.5)
-            {
-                DetectedLanguageLabel.Content = languageInfo["language"];
-                return languageInfo["language"];
-            }
-            else
-                return "Unable to confidently detect input language.";
-        }
+        
         // NOTE:
         // In the following sections, we'll add code below this.
         // ***** CORRECT SPELLING OF TEXT TO BE TRANSLATED
-        private string CorrectSpelling(string text)
-        {
-            string uri = BING_SPELL_CHECK_API_ENDPOINT + "?mode=spell&mkt=en-US";
-
-            // Create a request to Bing Spell Check API
-            HttpWebRequest spellCheckWebRequest = (HttpWebRequest)WebRequest.Create(uri);
-            spellCheckWebRequest.Headers.Add("Ocp-Apim-Subscription-Key", COGNITIVE_SERVICES_KEY);
-            spellCheckWebRequest.Headers.Add("Ocp-Apim-Subscription-Region", "westeurope");
-            spellCheckWebRequest.Method = "POST";
-            spellCheckWebRequest.ContentType = "application/x-www-form-urlencoded"; // doesn't work without this
-
-            // Create and send the request
-            string body = "text=" + System.Web.HttpUtility.UrlEncode(text);
-            byte[] data = Encoding.UTF8.GetBytes(body);
-            spellCheckWebRequest.ContentLength = data.Length;
-            using (var requestStream = spellCheckWebRequest.GetRequestStream())
-                requestStream.Write(data, 0, data.Length);
-            HttpWebResponse response = (HttpWebResponse)spellCheckWebRequest.GetResponse();
-
-            // Read and parse the JSON response; get spelling corrections
-            var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
-            var responseStream = response.GetResponseStream();
-            var jsonString = new StreamReader(responseStream, Encoding.GetEncoding("utf-8")).ReadToEnd();
-            dynamic jsonResponse = serializer.DeserializeObject(jsonString);
-            var flaggedTokens = jsonResponse["flaggedTokens"];
-
-            // Construct sorted dictionary of corrections in reverse order (right to left)
-            // This ensures that changes don't impact later indexes
-            var corrections = new SortedDictionary<int, string[]>(Comparer<int>.Create((a, b) => b.CompareTo(a)));
-            for (int i = 0; i < flaggedTokens.Length; i++)
-            {
-                var correction = flaggedTokens[i];
-                var suggestion = correction["suggestions"][0];  // Consider only first suggestion
-                if (suggestion["score"] > (decimal)0.7)         // Take it only if highly confident
-                    corrections[(int)correction["offset"]] = new string[]   // dict key   = offset
-                        { correction["token"], suggestion["suggestion"] };  // dict value = {error, correction}
-            }
-
-            // Apply spelling corrections, in order, from right to left
-            foreach (int i in corrections.Keys)
-            {
-                var oldtext = corrections[i][0];
-                var newtext = corrections[i][1];
-
-                // Apply capitalization from original text to correction - all caps or initial caps
-                if (text.Substring(i, oldtext.Length).All(char.IsUpper)) newtext = newtext.ToUpper();
-                else if (char.IsUpper(text[i])) newtext = newtext[0].ToString().ToUpper() + newtext.Substring(1);
-
-                text = text.Substring(0, i) + newtext + text.Substring(i + oldtext.Length);
-            }
-            return text;
-        }
+        
         // NOTE:
         // In the following sections, we'll add code below this.
         // ***** PERFORM TRANSLATION ON BUTTON CLICK
-        //private async void TranslateButton_Click(object sender, EventArgs e)
-        //{
-        //    //string textToTranslate = TextToTranslate.Text.Trim();
-
-
-
-        //    //await translate(textToTranslate);
-
-        //    // Update the translation field
-        //    // TranslatedTextLabel.Content = translation;
-        //}
-        string translation;
-        private async System.Threading.Tasks.Task translate(string textToTranslate)
-        {
-            string fromLanguage = FromLanguageComboBox.SelectedValue.ToString();
-            string fromLanguageCode;
-
-            // auto-detect source language if requested
-            if (fromLanguage == "Detect")
-            {
-                fromLanguageCode = DetectLanguage(textToTranslate);
-                if (!languageCodes.Contains(fromLanguageCode))
-                {
-                    MessageBox.Show("The source language could not be detected automatically " +
-                        "or is not supported for translation.", "Language detection failed",
-                        MessageBoxButton.OK, MessageBoxImage.Error);
-                    return;
-                }
-            }
-            else
-                fromLanguageCode = languageCodesAndTitles[fromLanguage];
-
-            string toLanguageCode = languageCodesAndTitles[ToLanguageComboBox.SelectedValue.ToString()];
-
-            //// spell-check the source text if the source language is English
-            //if (fromLanguageCode == "en")
-            //{
-            //    if (textToTranslate.StartsWith("-"))    // don't spell check in this case
-            //        textToTranslate = textToTranslate.Substring(1);
-            //    else
-            //    {
-            //        textToTranslate = CorrectSpelling(textToTranslate);
-            //        TextToTranslate.Text = textToTranslate;     // put corrected text into input field
-            //    }
-            //}
-            // handle null operations: no text or same source/target languages
-            if (textToTranslate == "" || fromLanguageCode == toLanguageCode)
-            {
-                translation = textToTranslate;
-                return;
-            }
-
-            // send HTTP request to perform the translation
-            string endpoint = string.Format(TEXT_TRANSLATION_API_ENDPOINT, "translate");
-            string uri = string.Format(endpoint + "&from={0}&to={1}", fromLanguageCode, toLanguageCode);
-
-            System.Object[] body = new System.Object[] { new { Text = textToTranslate } };
-            var requestBody = JsonConvert.SerializeObject(body);
-
-            using (var client = new HttpClient())
-            using (var request = new HttpRequestMessage())
-            {
-                request.Method = HttpMethod.Post;
-                request.RequestUri = new Uri(uri);
-                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
-                request.Headers.Add("Ocp-Apim-Subscription-Key", COGNITIVE_SERVICES_KEY);
-                request.Headers.Add("Ocp-Apim-Subscription-Region", "westeurope");
-                request.Headers.Add("X-ClientTraceId", Guid.NewGuid().ToString());
-
-                var response = await client.SendAsync(request);
-                var responseBody = await response.Content.ReadAsStringAsync();
-
-                var result = JsonConvert.DeserializeObject<List<Dictionary<string, List<Dictionary<string, string>>>>>(responseBody);
-                translation = result[0]["translations"][0]["text"];
-
-
-            }
-        }
+       
+        
+        
         private async Task<string> GetXmlTranslated4Names(string xmlFile, string language)
         {
             string toLanguageCode = languageCodesAndTitles[language];
@@ -310,8 +120,8 @@ namespace MSTranslatorDemo
                     if (NameEndPtr != -1)
                     {
                         NameOriginalText = xmlFile.Substring(NameStartPtr + NameStartSearchTxt.Length, NameEndPtr - NameStartPtr - NameStartSearchTxt.Length);
-                        await translate(NameOriginalText);
-                        NameTranslatedText = translation;
+                        NameTranslatedText = await new LanguageClass().translate(NameOriginalText, FromLanguageComboBox.SelectedItem.ToString(), "English");
+                         
 
                         xmlFile = xmlFile.Replace(NameStartSearchTxt + NameOriginalText + NameEndSearchTxt, NameStartSearchTxt + NameTranslatedText + NameEndSearchTxt);
                     }
@@ -366,8 +176,8 @@ namespace MSTranslatorDemo
                                 {
                                     CountryOriginalText = xsltFile.Substring(CountryXsltStartPtr2 + 11, CountryXsltEndPtr - CountryXsltStartPtr2 - 11);
                                     //now translate xslt
-                                    await translate(CountryOriginalText);
-                                    CountryTranslatedText = translation;
+                                    CountryTranslatedText = await new LanguageClass().translate(CountryOriginalText, FromLanguageComboBox.SelectedItem.ToString(), "English");
+                                    
                                     CountryXsltStartPtr2 = xsltFile.IndexOf("<t id=\"no\">", CountryXsltEndPtr);
                                     if (CountryXsltStartPtr2 != -1)
                                     {
@@ -413,8 +223,8 @@ namespace MSTranslatorDemo
                 if (NoteEndPtr != -1)
                 {
                     NoteOriginalText = xmlFile.Substring(NoteStartPtr + NoteStartSearchTxt.Length, NoteEndPtr - NoteStartPtr - NoteStartSearchTxt.Length);
-                    await translate(NoteOriginalText);
-                    NoteTranslatedText = translation;
+                    NoteTranslatedText = await new LanguageClass().translate(NoteOriginalText, FromLanguageComboBox.SelectedItem.ToString(), "English");
+
 
                     xmlFile = xmlFile.Replace(NoteStartSearchTxt + NoteOriginalText + NoteEndSearchTxt, NoteStartSearchTxt + NoteTranslatedText + NoteEndSearchTxt);
                 }
@@ -423,58 +233,7 @@ namespace MSTranslatorDemo
             return xmlFile;
 
         }
-        private async Task<string> GetXsltTranslated4Labels(string xsltFile, string language)
-        {
-            string toLanguageCode = languageCodesAndTitles[language];
-            //<xsl:param name="language" select="'en'"/>
-            //string xsltFile = File.ReadAllText(xsltFilename);
-            string Labels2Translate = File.ReadAllText("Labels2Translate.txt");
-            await translate(Labels2Translate);
-
-            string[] Translatedlines = translation.Split(
-                new[] { Environment.NewLine },
-                StringSplitOptions.None
-                );
-
-            string[] originalLines = Labels2Translate.Split(
-                new[] { Environment.NewLine },
-                StringSplitOptions.None
-                );
-            int ot, st, et, intLabelStart, intCodeStart;
-            string string2replace, newstring;
-            //<t id="en">Invoice<
-            //<t id="no"> = start of replace
-            //</t> = end of replace
-            intLabelStart = xsltFile.IndexOf("<xsl:variable name=\"labels\">");
-            intCodeStart = xsltFile.IndexOf("<cl id=\"uncl1001invoice\">");
-            for (int i = 0; i < Translatedlines.Length; i++)
-            {
-
-                ot = xsltFile.IndexOf("<t id=\"en\">" + originalLines[i] + "</t>", intLabelStart);
-                if (ot == -1)
-                    ot = xsltFile.IndexOf("<t id=\"en\">" + originalLines[i] + "</t>", intCodeStart);
-                if (ot == -1)
-                    ot = xsltFile.IndexOf("<t id=\"en\">" + originalLines[i] + "</t>", 0);
-                if (ot == -1)
-                { }
-                else
-                {
-                    st = xsltFile.IndexOf("<t id=\"no\">", ot);
-                    et = xsltFile.IndexOf("</t>", st);
-                    string2replace = xsltFile.Substring(st, et - st + 4);
-                    newstring = "<t id=\"" + toLanguageCode + "\">" + Translatedlines[i] + "</t>";
-                    xsltFile = xsltFile.Replace(string2replace, newstring);
-                }
-
-
-            }
-
-            xsltFile = xsltFile.Replace("<xsl:param name=\"language\" select=\"'en'\"/>", "<xsl:param name=\"language\" select=\"'" + toLanguageCode + "'\"/>");
-
-            return xsltFile;
-
-
-        }
+        
 
 
         private async void btnLoadXML_Click(object sender, RoutedEventArgs e)
@@ -501,7 +260,7 @@ namespace MSTranslatorDemo
                 
                 string OriginalxsltFile = File.ReadAllText("stylesheet-ubl v2.xslt");
 
-                string result = await GetXsltTranslated4Labels(OriginalxsltFile, ToLanguageComboBox.Text);
+                string result = await new LanguageClass().GetXsltTranslated4Labels(OriginalxsltFile, ToLanguageComboBox.Text, "English" );
                 result = await GetXsltTranslated4CountryID(OriginalxmlFile, result, ToLanguageComboBox.Text);
                 File.WriteAllText(ToLanguageComboBox.Text + "-stylesheet-ubl.xslt", result);
                 //  string HTMLstring = XLSThelper.TransformXMLToHTML(File.ReadAllText("cleaning services.xml"), File.ReadAllText("stylesheet-ubl.xslt"));
